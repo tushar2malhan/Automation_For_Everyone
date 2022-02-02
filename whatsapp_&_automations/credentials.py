9d3573d41e1e097750531ad994ba74263d329b3a
tushar malhan <66706496+tushar2malhan@users.noreply.github.com>
2022-02-02 13:00:59 +0530

    Delete credentials.py

--- whatsapp_&_automations/credentials.py
+++
@@ -1,7 +0,0 @@
-all_credentials = {
-    "linked_email":'tusharmalhan@gmail.com',
-    "linked_password":'tushar2399',
-    "fortclient_password":'PassTp!@3',
-    "fb_email":'tusharmalhan@gmail.com',
-    "fb_password":'tushar@21',
-}
